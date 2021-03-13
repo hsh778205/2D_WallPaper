1e29696f9f0e8e46c1157a795323bf3fc2689ec2
何劭弘 <51521470+hsh778205@users.noreply.github.com>
2021-03-13 11:17:53 +0800

    fix second bug today

--- CLD.py
+++ CLD.py
@@ -2,7 +2,7 @@ import skimage.io as io
 import time
 st=time.clock()
 import matplotlib.pyplot as plt
-fp=open("md5.txt","r")
+fp=open("tempname.txt","r")
 done=False
 name=fp.readline()
 fp.close()
@@ -18,15 +18,20 @@ fp=open("CLD.txt","a")
 hmax=int(h/8)
 wmax=int(w/8)
 cnt=0
-for si in range(0,h,hmax):
-    for sj in range(0,w,wmax):
+fp.write(name+"  ")
+for si in range(0,8):
+    for sj in range(0,8):
         Sum = [0, 0, 0]
         for i in range(0,hmax):
             for j in range(0,wmax):
-                if(i+si<h and j+sj<w):
-                    Sum[0]+=img[i+si,j+sj,0]
-                    Sum[1] += img[i+si,j+sj, 1]
-                    Sum[2] += img[i+si,j+sj, 2]
+                if(i+si*8<h and j+sj*8<w):
+                    Sum[0]+=img[i+si*8,j+sj*8,0]
+                    Sum[1] += img[i+si*8,j+sj*8, 1]
+                    Sum[2] += img[i+si*8,j+sj*8, 2]
         fp.write(str(int(Sum[0]/hmax/wmax))+" "+str(int(Sum[1]/hmax/wmax))+" "+str(int(Sum[2]/hmax/wmax))+" ")
+        cnt=cnt+1
 fp.write("\n")
 print('Running time: %s Seconds'%(time.clock()-st))
+print(cnt)
+if(cnt!=64):
+    print("????? cnt="+(str)(cnt))
